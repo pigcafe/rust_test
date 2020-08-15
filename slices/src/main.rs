de998682a63f550f28cea2907d8021c660751a0a
pigcafe <nexusdevcafe@gmail.com>
2020-08-16 01:13:10 +0900

    [update] understanding slices/src/main.rs

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -5,17 +5,21 @@ fn main() {
     //let world = &s[6..11];
     //println!("{}{}", hello, world);
 
-    let word = first_word(&s);
+    let word = first_word(&s[..]);
 
     println!("{}", word);
 
-    // FIXME: No errors occurs.
-    //   In the doc of rust, an error should be happed.
+    // NOTE: No errors occurs.
+    //   In the doc of rust, an error should be happened.
+    //   However, if anyone no use it after here, no error happened at this line.
     //   Environment: rustc 1.45.0 (5c1f21c3b 2020-07-13) running on Ubuntu-20.04
     s.clear();
+
+    println!("{}", s);
+    //println!("{}", word);     <-- Error.
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
